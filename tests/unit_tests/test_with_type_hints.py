95ff359728b61ed77b29cce30e4146763ab4a5a6
billydaly-qbiz <billy.daly@qbizinc.com>
2023-05-03 09:14:54 -0400

    test: Add tests for word_count functions

---
+++ tests/unit_tests/test_with_type_hints.py
@@ -0,0 +1,36 @@
+from word_counter.with_type_hints import CountResults, Counts, word_count
+
+
+def test_total_counts_matches_unique_counts():
+    """Total and unique counts should match when all words and chars are unique"""
+    # setup
+    expected = CountResults(
+        words=Counts(total=2, unique=2),
+        chars=Counts(total=7, unique=7),
+    )
+    # execution
+    output = word_count("abc def")
+    # validation - word counts
+    assert output.words.total == output.words.unique
+    assert output.words.total == expected.words.total
+    assert output.words.unique == expected.words.unique
+    # validation - char counts
+    assert output.chars.total == output.chars.unique
+    assert output.chars.total == expected.chars.total
+    assert output.chars.unique == expected.chars.unique
+
+
+def test_total_counts_different_from_unique_counts():
+    """Total and unique counts should be different when words are repeated"""
+    # setup
+    expected = CountResults(
+        words=Counts(total=2, unique=1),
+        chars=Counts(total=7, unique=4),
+    )
+    # execution
+    output = word_count("abc abc")
+    # validation - word counts
+    assert output.words.total > output.words.unique
+    assert output.words.total == expected.words.total
+    assert output.words.unique == expected.words.unique
+    # validation - char counts
